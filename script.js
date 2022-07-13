2fde7aa435749041d64d65f8757206c0734372de
agusgonz <agustingendeiza@gmail.com>
2022-07-13 10:47:19 -0300

    Mejorando el canvas

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 var canvas = document.getElementById(canvas);
 var c = canvas.getContext("2d");
+
+function dibujLinea(color, xI, yI, xF, yF){
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xI, yI);
+    lienzo.lineTo(xF, yF);
+    lienzo.stroke();
+    lienzo.closePath(); 
+
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(yI, xI);
+    lienzo.lineTo(yF, xF);
+    lienzo.stroke();
+    lienzo.closePath(); 
+}
